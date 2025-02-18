3402d900ee624b6016538eb11ea39e7f1bb187a6
Bissens22 <tobiasstenmandhansen@gmail.com>
2025-02-18 11:09:23 +0100

    Day2.2

--- Test.ps1
+++ Test.ps1
@@ -1,15 +1,29 @@
 $Address = Read-Host "Specify the ip address of the machine you want to establish a connection with"
+if ($Address -eq "") {
+    Break
+}
 
 winrm quickconfig
-Set-Item wsman:\localhost\Client\TrustedHosts -Value "$Address"
+Set-Item wsman:\localhost\Client\TrustedHosts -Value "$Address" -Force
 Get-Item wsman:\localhost\Client\TrustedHosts
 Restart-Service WinRM
 $Session = New-PSSession -ComputerName "$Address" -Credential Administrator
 
-
+$choice = Read-Host "If you want to create a Local-admin, Type -Locala, If you want to do nothing type -Exit"
+if ($choice -eq "Locala") {
+    $Localadmin
+}
+if ($choice -eq "Exit") {
+    break
+}
+if ($Choice -ne "Exit", "Locala") {
+    Break
+}
 
 Invoke-command -Session $Session -ScriptBlock {
-function Create-NewLocalAdmin {
+
+
+    function Create-NewLocalAdmin {
     [CmdletBinding()]
     param (
         [string] $NewLocalAdmin,
@@ -28,6 +42,6 @@ function Create-NewLocalAdmin {
 }
 $NewLocalAdmin = Read-Host "New local admin username:"
 $Password = Read-Host -AsSecureString "Create a password for $NewLocalAdmin"
-Create-NewLocalAdmin -NewLocalAdmin $NewLocalAdmin -Password $Password -Verbose
+$Localadmin = Create-NewLocalAdmin -NewLocalAdmin $NewLocalAdmin -Password $Password -Verbose
 net localgroup "Administrators"
 }
